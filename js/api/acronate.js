9bb6203072899f4ca8309992b4186d4c88387dcc
Fireforge <fireforge124@gmail.com>
2014-04-12 14:09:34 +0000

    api code initial commit

---
+++ js/api/acronate.js
@@ -0,0 +1,27 @@
+/**
+* Created with Acronator.
+* User: Fireforge
+* Date: 2014-04-12
+* Time: 01:45 PM
+* To change this template use Tools | Templates.
+*/
+function acronate( acronym, keywords )
+	Create an empty list of PossibleWords associated with each letter in acronym
+	
+    for (keyword in keywords):
+		if the word can be used in the acronym:
+			put it in the appropriate list with priority 0
+		get a list of single word synonyms of the keyword
+		for each synonym:
+			if the synonym can be used in the acronym:
+				put it in the appropriate list with priority 1
+		for each filler word:
+			if it can be used:
+				include w/ priority 2
+
+Return all combinations of PossibleWords between lists which minimize total priority
+			
+class PossibleWord
+	word
+	priority
+	source_keyword
