c7d5e30284fb01145f46183e3eb8c4ef1fc53550
Fireforge <fireforge124@gmail.com>
2014-04-12 17:36:05 +0000

    First attmept as real acronation logic

--- js/api/acronate.js
+++ js/api/acronate.js
@@ -7,41 +7,75 @@
 */
 
 function acronate( acronym, keywords ){
+    console.log(">>>acronate")
+    // Setup the 
     var acronymLetters = [];
     for ( var i = 0; i < acronym.length; i++ )
     {
-        acronymLetters.push(AcronymLetter(acronym.charAt(i), ['hello']))
+        acronymLetters.push(AcronymLetter(acronym.charAt(i), []))
     }
+    console.log(acronymLetters);
+    
+    // Get the synonyms from the bighugelabs API
+    keywordsArray = keywords.split(" ");
+    console.log(keywordsArray);
+    var wordBank = [];
+    
+    var ajaxReqs = [];
+    for (var j = 0; j < keywordsArray.length; j++){
+        ajaxReqs.push($.ajax({
+            url: 'https://words.bighugelabs.com/api/2/ff854eb1f0151b1a2d15940fdb5cb1b5/' + keywordsArray[j] + '/json',
+            dataType:'jsonp',
+            //async: 'false',
+            success: function(response) {
+                for (syntype in response){
+                    console.log(response[syntype].syn);
+                    wordBank = wordBank.concat(response[syntype].syn)
+                }
+            }
+        }));
+    }
+    //console.log(ajaxReqs);
+    // when all AJAX requests are complete
+    $.when.apply($, ajaxReqs).then(function() {
+        //console.log(wordBank);
 
-
-    $.ajax({
-        url: 'http://words.bighugelabs.com/api/2/ff854eb1f0151b1a2d15940fdb5cb1b5/word/json',
-        data: keywords[0],
-        method: 'POST',
-        dataType:'jsonp',
-        success: function(response) {
-        	console.log('AJ');
-            console.log(response.noun.syn[0]);
-            //use data here.
+        // Sort all words in word bank with their matching acronym letters
+        for (var j = 0; j < acronymLetters.length; j++){
+            for (word in wordBank){
+                console.log(word);
+                if (acronymLetters[j].checkword(acronymLetters[j], wordBank[word])){
+                    acronymLetters[j].words.push(wordBank[word]);
+                    //possibleAcronym.push(wordBank[word]);
+                }
+            }
         }
+        
+        // Now that we've sorted the synonyms, figure out some de-acronizations
+        var deacronizations = [];
+        for (var i = 0; i < 5; i++){ //hardcoded to 5 deacros for now
+            var deacro = []
+            for (var j = 0; j < acronymLetters.length; j++){
+                var letter = acronymLetters[j];
+                var word = letter.words[Math.floor(Math.random()*letter.words.length)];
+                deacro.push(word);
+            }
+            deacronizations.push(deacro)
+        }
+        console.log(deacronizations);
+        console.log("<<<acronate")
+        return deacronizations;
     });
-
-    var firstAcronym = [];
-    for (var j = 0; j < acronymLetters.length; j++){
-    	//console.log(acronymLetters[j]);
-        firstAcronym.push(acronymLetters[j].words[0]);
-        //possibleAcronyms.push(l.join(" "))
-    }
-    return [firstAcronym];
 }
 
 function AcronymLetter(letter, words) {
-    this.letter = letter;
-	this.words = words;
+    var myobj = new Object();
+    myobj.letter = letter;
+    myobj.words = words;
 
-    this.checkword = function(word){
-        return word.charAt(0) == this.letter;
+    myobj.checkword = function(obj, word){
+        return word.charAt(0) == obj.letter;
     }
 
-    return this;
+    return myobj;
 }
